623ee19e75f8f710537b702d2659a743fd17448c
Monika Kedrova <6909854+m-ke@users.noreply.github.com>
2018-12-28 19:31:27 +0100

    Add sub-navbar to dashboard

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <main-header/>
+    <div id="large-sub-header" class="float-right">
+      <button
+        class="m-2"
+        @click="newPkg()">
+        Add New Valuation
+      </button>
+    </div>
     <slot/>
   </div>
 </template>
 
 <script>
 import MainHeader from '@/components/MainHeader';
+import { router } from '@/router'
 
 export default {
   components: {
     MainHeader
+  },
+  methods: {
+    newPkg () {
+      // TODO generate ID & create new package
+      router.push('/package/id/property-info')
+    }
   }
-};
+}
 </script>
