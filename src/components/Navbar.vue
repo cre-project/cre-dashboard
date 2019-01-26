41b8b3f4f1c4ca8a10850d45da7a7a37671834ac
Monika Kedrova <6909854+m-ke@users.noreply.github.com>
2019-01-26 14:32:23 +0100

    Make navbar links clickable again

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,30 @@
 <template>
   <nav class="navbar" role="navigation">
     <ul id="bottom" class="navbar-menu">
-      <li class="navbar-item is-hoverable" v-for="route in r" :key="route.name">
-        {{ route.name.toUpperCase() }}
-        <!-- <router-link :to="route.path">{{ route.name.toUpperCase() }}</router-link> -->
+      <li class="navbar-item is-hoverable" v-for="r in routes" :key="r.name">
+        <router-link :to="path(r.path)">{{ r.name.toUpperCase() }}</router-link>
       </li>
     </ul>
   </nav>
 </template>
 <script>
-import { routes } from '@/router'
+import { routes, router } from '@/router'
 
 export default {
   computed: {
-    r () {
+    routes () {
       return routes.filter(route => route.meta && route.meta.isNav)
+    }
+  },
+  methods: {
+    path (p) {
+      if (this.$route.params.id) {
+        return p.replace(':id', this.$route.params.id)
+      } else if (p.indexOf(':id') !== -1) {
+        // no id in params but id in path is required => redirect to dashboard
+        router.push('/')
+      }
+      return p
     }
   }
 }
